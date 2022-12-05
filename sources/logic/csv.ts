b7b75ab6fa1a5399cf463ecb75b6d2e5b5a61179
Alexander Koryttsev <akoryttsev@gmail.com>
2022-12-05 09:02:26 +0100

    Small fixes

--- sources/logic/csv.ts
+++ sources/logic/csv.ts
@@ -8,43 +8,45 @@ export const shareCSV = async (months: string[]) => {
 	if (!userStore.me) return
 	// enumerate months
 	let csv = ''
-	months.forEach((month) => {
-		// | month
-		// | create string
-		csv += month + '\n'
-		csv += '🗓'
-		const { wakeUpEnabled, serviceEnabled, yogaEnabled, lectionsEnabled, bedEnabled } =
-			settingsStore
+	months
+		.sort((a, b) => a.localeCompare(b))
+		.reverse()
+		.forEach((month) => {
+			// | month
+			// | create string
+			csv += month
+			const { wakeUpEnabled, serviceEnabled, yogaEnabled, lectionsEnabled, bedEnabled } =
+				settingsStore
 
-		// | add header (based on ther user options)
-		if (wakeUpEnabled) csv += ',☀️'
-		csv += ',🎹,📖'
-		if (serviceEnabled) csv += ',🦶'
-		if (yogaEnabled) csv += ',🧘‍♂️'
-		if (lectionsEnabled) csv += ',🎧'
-		if (bedEnabled) csv += ',🌙'
-		csv += ',7:30,10:00,18:00,00:00'
+			// | add header (based on ther user options)
+			if (wakeUpEnabled) csv += ',☀️'
+			csv += ',🎹,📖'
+			if (serviceEnabled) csv += ',🦶'
+			if (yogaEnabled) csv += ',🧘‍♂️'
+			if (lectionsEnabled) csv += ',🎧'
+			if (bedEnabled) csv += ',🌙'
+			csv += ',7:30,10:00,18:00,00:00'
 
-		// | fetch entries
-		const entries = graphStore.my!.entriesByMonth[month]
-		// | enumerate entries
-		entries.forEach((entry) => {
-			csv += '\n' + entry.day
-			if (wakeUpEnabled) csv += ',' + (entry.opt_wake_up ?? '')
-			csv += ',' + (entry.kirtan ? '✓' : '') + ',' + (entry.reading ?? '')
-			if (serviceEnabled) csv += ',' + (entry.opt_service === '1' ? '✓' : '')
-			if (yogaEnabled) csv += ',' + (entry.opt_service === '1' ? '✓' : '')
-			if (lectionsEnabled) csv += ',' + (entry.opt_lections === '1' ? '✓' : '')
-			if (bedEnabled) csv += ',' + (entry.opt_sleep ?? '')
+			// | fetch entries
+			const entries = graphStore.my!.entriesByMonth[month]
+			// | enumerate entries
+			entries.forEach((entry) => {
+				csv += '\n' + entry.day
+				if (wakeUpEnabled) csv += ',' + (entry.opt_wake_up ?? '')
+				csv += ',' + (entry.kirtan === '1' ? '✓' : '') + ',' + (entry.reading ?? '')
+				if (serviceEnabled) csv += ',' + (entry.opt_service === '1' ? '✓' : '')
+				if (yogaEnabled) csv += ',' + (entry.opt_service === '1' ? '✓' : '')
+				if (lectionsEnabled) csv += ',' + (entry.opt_lections === '1' ? '✓' : '')
+				if (bedEnabled) csv += ',' + (entry.opt_sleep ?? '')
 
-			csv +=
-				formatRounds(entry.jcount_730) +
-				formatRounds(entry.jcount_1000) +
-				formatRounds(entry.jcount_1800) +
-				formatRounds(entry.jcount_after)
+				csv +=
+					formatRounds(entry.jcount_730) +
+					formatRounds(entry.jcount_1000) +
+					formatRounds(entry.jcount_1800) +
+					formatRounds(entry.jcount_after)
+			})
+			csv += '\n'
 		})
-		csv += '\n'
-	})
 
 	// share csvs
 	// TODO: think about pretty name
